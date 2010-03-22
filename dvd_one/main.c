640f938adb4a2b9d5ea5461b837fbacd402edd60
reddavis <reddavis@gmail.com>
2010-03-22 19:35:27 +0000

    gitignore

--- dvd_one/main.c
+++ dvd_one/main.c
@@ -3,17 +3,25 @@
 #define k_max_dvds 5
 #define k_max_title_length 256
 #define k_max_comment_length 256
+#define k_error_fgets -1
 
 void PrintDVDTitle(int dvd_num, char titles[][k_max_title_length]);
 
 int main (int argc, const char * argv[]) {
   short dvd_number;
   char titles[k_max_dvds][k_max_title_length];
-    
+  char *result;
+  
   for(dvd_number = 0; dvd_number < k_max_dvds; dvd_number++) {
     printf("Title of DVD #%d: ", dvd_number+1);
     // Fill title[dvd] with what it reads from stdin. Max length: k_max_title_length
-    fgets(titles[dvd_number], k_max_title_length, stdin);
+    result = fgets(titles[dvd_number], k_max_title_length, stdin);
+    
+    // fgets returns NULL if error
+    if(result == NULL) {
+      printf("---ERROR---");
+      return k_error_fgets;
+    }
   }
   
   printf("-----\n");
